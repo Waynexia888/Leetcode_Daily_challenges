e017f284b6f87c342c50579cab34f8926f30241b
Waynexia888 <qiwuwaynexia@gmail.com>
2021-01-06 15:03:33 -0800

    Leetcode 266 Palindrome Permutation 2nd method

--- 266. Palindrome Permutation.java
+++ 266. Palindrome Permutation.java
@@ -1,3 +1,4 @@
+// time: O(n); space: O(n)
 class Solution {
     public boolean canPermutePalindrome(String s) {
         HashSet<Character> set = new HashSet<>();
@@ -9,5 +10,30 @@ class Solution {
             }
         }
         return set.size() <= 1;
+    }
+}
+
+
+// time: O(n), space: O(1)
+
+class Solution {
+    public boolean canPermutePalindrome(String s) {
+        char[] count = new char[256];
+        
+        for (char c : s.toCharArray()) {
+            if (count[c] > 0) {
+                count[c]--;
+            } else {
+                count[c]++;
+            }
+        }
+        
+        int result = 0;
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] != 0) {
+                result++;
+            }
+        }
+        return result <= 1;
     }
 }
